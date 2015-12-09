3c89e983559b939cce1286e36165b30a06f0ee8c
Huong <huong.doan@precisbeusiness.com.au>
2015-12-09 17:11:43 +0700

    xxx

---
+++ src/AngularJSSuite/Models/AccountsApiModel.cs
@@ -0,0 +1,38 @@
+﻿using AngularJSSuite.Service.Entities;
+using AngularJSSuite.Services.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularJSSuite.Models
+{
+    public class AccountsApiModel : TransactionalInformation
+    {
+        public List<ApplicationMenu> MenuItems;
+        public User User;
+
+        public AccountsApiModel()
+        {
+            User = new User();
+            MenuItems = new List<ApplicationMenu>();
+        }
+
+    }
+
+    public class UserDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirmation { get; set; }
+    }
+
+    public class LoginUserDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
